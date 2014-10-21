27f8e5b93aa5b72f5a759a4fa6b74a5cbe708c66
Michael Krasnow <mnkras@gmail.com>
2014-10-21 14:52:15 -0400

    Indent!

--- controller.php
+++ controller.php
@@ -3,40 +3,45 @@ namespace Concrete\Package\CustomMenuItems;
 
 defined('C5_EXECUTE') or die("Access Denied.");
 
-use Concrete\Core\Block\BlockType\BlockType;
-
 /**
-*
-* Custom Menu Items Package.
-* @author Michael Krasnow <mnkras@gmail.com>
-*
-*/
+ *
+ * Custom Menu Items Package.
+ * @author Michael Krasnow <mnkras@gmail.com>
+ *
+ */
 
-class Controller extends \Concrete\Core\Package\Package {
+class Controller extends \Concrete\Core\Package\Package
+{
 
-	protected $pkgHandle = 'custom_menu_items';
-	protected $appVersionRequired = '5.7.0.4';
-	protected $pkgVersion = '0.9';
+    protected $pkgHandle = 'custom_menu_items';
+    protected $appVersionRequired = '5.7.0.4';
+    protected $pkgVersion = '0.9';
 
-	public function getPackageDescription() {
-		return t("Create menu items to any page on your site.");
-	}
+    public function getPackageDescription()
+    {
+        return t("Create menu items to any page on your site.");
+    }
 
-	public function getPackageName() {
-		return t("Custom Menu Items");
-	}
+    public function getPackageName()
+    {
+        return t("Custom Menu Items");
+    }
 
-	public function install() {
-		$pkg = parent::install();
+    public function install()
+    {
+        $pkg = parent::install();
         $sp = \SinglePage::add('/dashboard/system/basics/custom_menu_items', $pkg);
-        if (is_object($sp)) {
+        if (is_object($sp))
+        {
             $sp->update(array('cName'=>t('Custom Menu Items')));
         }
     }
 
-	public function on_start() {
-		$u = new \User();
-		if($u->isLoggedIn()) {
+    public function on_start()
+    {
+        $u = new \User();
+        if($u->isLoggedIn())
+        {
             $db = \Loader::db();
             $r = $db->Execute('SELECT * FROM pkgCustomMenuItems ORDER BY DisplayOrder');
             while($row = $r->fetchRow())
@@ -52,8 +57,6 @@ class Controller extends \Concrete\Core\Package\Package {
                     )
                 );
             }
-            //var_dump($menu->getPageHeaderMenuItems());
-
-		}
-	}
+        }
+    }
 }
